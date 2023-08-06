5afb816648d0e3aeb98d35f42961e167fa9c074a
Gabe Yuan <yugang2002@gmail.com>
2023-08-06 23:30:00 +0800

    remove options script

--- src/userscriptOptions.js
+++ src/userscriptOptions.js
@@ -1,45 +1,45 @@
-import React, { useEffect, useState } from "react";
-import ReactDOM from "react-dom/client";
+// import React, { useEffect, useState } from "react";
+// import ReactDOM from "react-dom/client";
 
-const App = () => {
-  const [loading, setLoading] = useState(true);
+// const App = () => {
+//   const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      if (!document.querySelector("header")) {
-        setLoading(false);
-      }
-    }, 5000);
-    return () => {
-      clearTimeout(timer);
-    };
-  }, []);
+//   useEffect(() => {
+//     const timer = setTimeout(() => {
+//       if (!document.querySelector("header")) {
+//         setLoading(false);
+//       }
+//     }, 5000);
+//     return () => {
+//       clearTimeout(timer);
+//     };
+//   }, []);
 
-  if (loading) {
-    return (
-      <center>
-        <p>KISS Translator</p>
-        <h1 style={{ textAlign: "center" }}>loading...</h1>
-      </center>
-    );
-  }
+//   if (loading) {
+//     return (
+//       <center>
+//         <p>KISS Translator</p>
+//         <h1 style={{ textAlign: "center" }}>loading...</h1>
+//       </center>
+//     );
+//   }
 
-  return (
-    <center>
-      <p>
-        <a href={process.env.REACT_APP_HOMEPAGE}>KISS Translator</a> Script not
-        installed or disabled!
-      </p>
-      <h1>
-        <a href={process.env.REACT_APP_HOMEPAGE}>Click here read more!</a>
-      </h1>
-    </center>
-  );
-};
+//   return (
+//     <center>
+//       <p>
+//         <a href={process.env.REACT_APP_HOMEPAGE}>KISS Translator</a> Script not
+//         installed or disabled!
+//       </p>
+//       <h1>
+//         <a href={process.env.REACT_APP_HOMEPAGE}>Click here read more!</a>
+//       </h1>
+//     </center>
+//   );
+// };
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
