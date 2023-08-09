0cc3f4f35617ba30fa58bbec624097c25e35c2a9
Gabe Yuan <gabe@zq.com>
2023-08-09 17:39:15 +0800

    new version

--- docs/kiss-translator.user.js
+++ docs/kiss-translator.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name          KISS Translator
 // @namespace     https://github.com/fishjar/kiss-translator
-// @version       1.3.8
+// @version       1.3.9
 // @description   A minimalist bilingual translation extension.
 // @author        Gabe<yugang2002@gmail.com>
 // @homepageURL   https://github.com/fishjar/kiss-translator
