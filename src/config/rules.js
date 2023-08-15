f43db8a6cafba05dd35fb4e5f769b1a2e12e76b1
Gabe Yuan <yugang2002@gmail.com>
2023-08-15 22:59:21 +0800

    update rules

--- src/config/rules.js
+++ src/config/rules.js
@@ -125,7 +125,7 @@ export const RULES = [
   },
   {
     pattern: `youtube.com`,
-    selector: `h1, h3:not(.ytd-comment-renderer), #content-text, #description, yt-attributed-string>span>span`,
+    selector: `h1, #video-title, #content-text, #description, yt-attributed-string>span>span`,
   },
   {
     pattern: `www.google.com/search`,
