7a3df25521e409e2b1d7c5f7ddc1aa07becf030a
Gabe Yuan <gabe@zq.com>
2023-08-29 10:41:20 +0800

    generate version file to web

--- src/rules.js
+++ src/rules.js
@@ -3,6 +3,7 @@ import path from "path";
 import { BUILTIN_RULES } from "./config/rules";
 
 (() => {
+  // rules
   try {
     const data = JSON.stringify(BUILTIN_RULES, null, "  ");
     const file = path.resolve(
@@ -11,6 +12,16 @@ import { BUILTIN_RULES } from "./config/rules";
     );
     fs.writeFileSync(file, data);
     console.info(`Built-in rules generated: ${file}`);
+  } catch (err) {
+    console.error(err);
+  }
+
+  // version
+  try {
+    var pjson = require("../package.json");
+    const file = path.resolve(__dirname, "../build/web/version.txt");
+    fs.writeFileSync(file, pjson.version);
+    console.info(`Version file generated: ${file}`);
   } catch (err) {
     console.error(err);
   }
