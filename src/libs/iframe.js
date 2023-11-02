06f93c1c108531166314765f4ddf8d45175d521a
Gabe Yuan <gabe@zq.com>
2023-11-02 10:56:46 +0800

    repace querySelectorAll to getElementsByName

--- src/libs/iframe.js
+++ src/libs/iframe.js
@@ -1,7 +1,7 @@
 export const isIframe = window.self !== window.top;
 
 export const sendIframeMsg = (action, args) => {
-  document.querySelectorAll("iframe").forEach((iframe) => {
+  document.getElementsByName("iframe").forEach((iframe) => {
     iframe.contentWindow.postMessage({ action, args }, "*");
   });
 };
