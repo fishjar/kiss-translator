84de1e0f1254f0c1c94d781b4a806998ab7eabc0
Gabe Yuan <gabe@zq.com>
2023-11-02 10:59:26 +0800

    replace getElementsByName to getElementsByTagName

--- src/libs/iframe.js
+++ src/libs/iframe.js
@@ -1,7 +1,7 @@
 export const isIframe = window.self !== window.top;
 
 export const sendIframeMsg = (action, args) => {
-  document.getElementsByName("iframe").forEach((iframe) => {
+  document.getElementsByTagName("iframe").forEach((iframe) => {
     iframe.contentWindow.postMessage({ action, args }, "*");
   });
 };
