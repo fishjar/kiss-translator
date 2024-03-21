e13da2caba95c3d9d7f13e5f8f3f43365bb4cd2a
Gabe Yuan <gabe@zq.com>
2024-03-21 16:07:56 +0800

    fix: i18n text

--- src/config/i18n.js
+++ src/config/i18n.js
@@ -452,8 +452,8 @@ export const I18N = {
     en: `Fixer Function`,
   },
   fixer_function_helper: {
-    zh: `1、br是将<br>换行替换成<p>包裹换行。2、bn是将\\n换行替换成<p>包裹换行。3、brToDiv和bnToDiv是替换成<div class="kiss-p">`,
-    en: `1. The goal of br is to replace <br> line breaks with <p> wrapped line breaks. 2. The goal of bn is to replace \\n newlines with <p> wrapped newlines. 3. brToDiv and bnToDiv are replaced with <div class="kiss-p">`,
+    zh: `1、br是将<br>换行替换成<p "kiss-p">。2、bn是将\\n换行替换成<p "kiss-p">。3、brToDiv和bnToDiv是替换成<div class="kiss-p">`,
+    en: `1. br replaces <br> line breaks with <p "kiss-p">. 2. bn replaces \\n newline with <p "kiss-p">. 3. brToDiv and bnToDiv are replaced with <div class="kiss-p">`,
   },
   import: {
     zh: `导入`,
