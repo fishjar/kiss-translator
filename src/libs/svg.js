549b945d0f7f513e9a1e6230057f370bf75d631e
Gabe <yugang2002@gmail.com>
2025-10-11 00:19:48 +0800

    fix: svg

--- src/libs/svg.js
+++ src/libs/svg.js
@@ -1,5 +1,4 @@
-export const loadingSvg = `
-<svg viewBox="-20 0 100 100" 
+export const loadingSvg = `<svg viewBox="-20 0 100 100" 
      style="display: inline-block; width: 1em; height: 1em; vertical-align: middle;">
   <circle fill="#209CEE" stroke="none" cx="6" cy="50" r="6">
     <animateTransform attributeName="transform" dur="1s" type="translate" values="0 15 ; 0 -15; 0 15" repeatCount="indefinite" begin="0.1"/>
