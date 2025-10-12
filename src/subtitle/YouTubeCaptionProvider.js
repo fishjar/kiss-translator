f6d095d5332335f165d4f823cc64f4086d6fd897
Gabe <yugang2002@gmail.com>
2025-10-12 12:02:41 +0800

    fix: subtitle

--- src/subtitle/YouTubeCaptionProvider.js
+++ src/subtitle/YouTubeCaptionProvider.js
@@ -274,8 +274,6 @@ class YouTubeCaptionProvider {
         return;
       }
 
-      this.#showNotification(this.#i18n("starting_to_process_subtitle"));
-
       const captionTracks = await this.#getCaptionTracks(videoId);
       const captionTrack = this.#findCaptionTrack(captionTracks);
       if (!captionTrack) {
@@ -294,15 +292,22 @@ class YouTubeCaptionProvider {
         return;
       }
 
-      const flatEvents = this.#flatEvents(events);
-      if (!flatEvents.length) return;
-
       const { segApiSetting, toLang } = this.#setting;
       const lang = potUrl.searchParams.get("lang");
       const fromLang =
         OPT_LANGS_TO_CODE[OPT_TRANS_MICROSOFT].get(lang) ||
         OPT_LANGS_TO_CODE[OPT_TRANS_MICROSOFT].get(lang.slice(0, 2)) ||
         "auto";
+
+      if (this.#isSameLang(fromLang, toLang)) {
+        logger.info("Youtube Provider: skip same lang", fromLang, toLang);
+        return;
+      }
+
+      this.#showNotification(this.#i18n("starting_to_process_subtitle"));
+
+      const flatEvents = this.#flatEvents(events);
+      if (!flatEvents.length) return;
 
       if (potUrl.searchParams.get("kind") === "asr" && segApiSetting) {
         logger.info("Youtube Provider: Starting AI ...");
