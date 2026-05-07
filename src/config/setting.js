a30b379831a2dd2425ae833ad1ec84648d63d5a4
Baneik <EssentialCuber@gmail.com>
2026-05-07 18:08:46 +0800

    feat: add a segmentation threshold setting option

--- src/config/setting.js
+++ src/config/setting.js
@@ -125,6 +125,7 @@ export const DEFAULT_SUBTITLE_SETTING = {
   apiSlug: OPT_TRANS_MICROSOFT,
   segSlug: "-", // AI智能断句
   chunkLength: 1000, // AI处理切割长度
+  longSentenceThreshold: 120, // 规则断句超长句阈值
   preTrans: 90, // 提前翻译时长
   throttleTrans: 30, // 节流翻译间隔
   // fromLang: "en",
